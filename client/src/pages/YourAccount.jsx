3b41bca50b330fafdc98b93db3d69c5cb606dcdf
Mohammed Talha <self-codex@users.noreply.github.com>
2021-07-08 13:33:29 +0500

    set profile items

--- client/src/pages/YourAccount.jsx
+++ client/src/pages/YourAccount.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { RiArrowRightSLine } from "react-icons/ri";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory, Link } from "react-router-dom";
-import UserProfile from "../components/profile/UserProfile";
+import UserProfile from "../components/userProfile/UserProfile";
 const YourAccount = () => {
   const { userAuth } = useSelector((state) => state.login);
   const history = useHistory();
@@ -25,7 +25,7 @@ const YourAccount = () => {
         <Link to="/your_account">your account</Link>
       </div>
       <div className="container-xl profile">
-        <h1>Your Account</h1>
+        <h1 className="profile__status">Your Account</h1>
         <UserProfile />
       </div>
     </>
