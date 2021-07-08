21543e4daf0ec685c550fd10d2385ef7336fb836
Mohammed Talha <self-codex@users.noreply.github.com>
2021-07-08 13:35:04 +0500

    profile item add

---
+++ client/src/components/userProfile/ProfileItem.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const ProfileItem = ({ Title, Icon, Text, Path }) => {
+  return (
+    <Link to={Path} className="profile__category">
+      <div className="profile__body">
+        <span>{Icon}</span>
+        <h1>{Title}</h1>
+      </div>
+      <p className="body__text">{Text}</p>
+    </Link>
+  );
+};
+
+export default ProfileItem;
